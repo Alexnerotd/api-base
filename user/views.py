e4afb5e6d00804dd9970ceb0687be535b0f684b7
Nero <jesalemalo@gmail.com>
2023-10-17 10:46:42 -0400

    I did add urls for token and create view register

--- user/views.py
+++ user/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from django.http import Http404
+
+
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
+
+from .models import MyUser
+from .serializers import *
 
 # Create your views here.
+
+
+class Register(APIView):
+
+
+    def post(self, request, format = None):
+        user_serializer = MyUserSerializerPOST(data=request.data)
+        if user_serializer.is_valid():
+            user_serializer.save()
+            return Response(user_serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response({"error":"Los datos ingresados no son validos"}, status=status.HTTP_400_BAD_REQUEST)
+        
+
+
+
