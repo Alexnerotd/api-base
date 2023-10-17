163aadf93993eeabc3bc25d2082ff329f86b4101
Nero <jesalemalo@gmail.com>
2023-10-17 10:53:13 -0400

    I fixed all configs in urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,8 @@
-from django import urls
+from django.urls import path, include
 
 
 urlpatterns = [
-    
+    path("", include('user.urls')),
+    path("", include('account.urls')),
+    path("", include('blog.urls')),
 ]
--- user/urls.py
+++ user/urls.py
@@ -6,9 +6,12 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from .views import (Register)
 
 urlpatterns = [
     
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    
+    path('api/register/', Register.as_view(), name='register'),
 ]
